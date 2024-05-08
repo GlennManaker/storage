1dc75cb1b89b471291e56cb34abe50c7da38ce9c
Uladzimir Filipchenkau <uladzimirfilipchenkau@MacBook-Pro-3.local>
2024-05-08 09:54:15 +0300

    fixes

--- main.go
+++ main.go
@@ -3,25 +3,41 @@ package main
 import (
 	"fmt"
 	"my_storage/pkg/storage"
+	"sync"
 	"time"
 )
 
 func main() {
+	var wg sync.WaitGroup
+
 	storage := storage.NewStorage[int, int]()
 	storage.Start()
 
 	now := time.Now()
 
-	fmt.Println(now)
+	wg.Add(2)
 
-	for t := range 20000000 {
-		storage.Set(t, -t, now.Add(time.Second*30))
-		//fmt.Println(storage.Get(key))
-	}
+	go func() {
+		defer wg.Done()
+		for t := range 2000000 {
+			storage.Set(t, -t, now.Add(time.Second*10))
+			//fmt.Println(storage.Get(key))
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for t := range 2000000 {
+			storage.Set(t+200000, -t-20000, now.Add(time.Second*10))
+			//fmt.Println(storage.Get(key))
+		}
+	}()
+
+	wg.Wait()
 
 	fmt.Println(time.Now().Sub(now))
 
-	time.Sleep(30 * time.Second)
+	time.Sleep(15 * time.Second)
 
-	fmt.Println(storage.GetRoot().GetRootValue())
+	fmt.Println(storage.GetRoot())
 }
